81974369d34878cadec49fb20e1c68e8785a6d23
darrdv <darr.dv@gmail.com>
2022-05-20 19:10:39 -0400

    Fetched monsters from api and displayed them

--- src/App.js
+++ src/App.js
@@ -9,10 +9,28 @@ class App extends Component {
     };
   }
 
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users/')
+    .then(response => response.json() )
+    .then(users => this.setState({monsters: users}))
+  }
+
   render(){
+    const {monsters} = this.state;
+    const displayedMonsters = monsters.map((monster) => {
+      return(
+        <div key={monster.id}>
+          <h1>{monster.name}</h1>
+        </div>
+      )
+    })
+
     return(
       <div className="App">
         <h1>Monster's Rolodex</h1>
+        <>
+          {displayedMonsters}
+        </>
       </div>
     );
   }
