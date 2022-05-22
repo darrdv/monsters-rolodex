37608c3d0187088bd38b9579dd9667fc1b4ee822
darrdv <darr.dv@gmail.com>
2022-05-21 22:01:45 -0400

    Created input search filtering

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
@@ -16,8 +17,11 @@ class App extends Component {
   }
 
   render(){
-    const {monsters} = this.state;
-    const displayedMonsters = monsters.map((monster) => {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField)
+   })
+    //const {monsters} = this.state;
+    const displayedMonsters = filteredMonsters.map((monster) => {
       return(
         <div key={monster.id}>
           <h1>{monster.name}</h1>
@@ -28,6 +32,19 @@ class App extends Component {
     return(
       <div className="App">
         <h1>Monster's Rolodex</h1>
+        <input
+         className='search-box'
+         type='search'
+         placeholder='Search Monsters'
+         onChange={ (event) => {
+           console.log(event.target.value)
+           const searchField = event.target.value.toLocaleLowerCase()
+           
+           this.setState(() => {
+             return {searchField}
+           })
+         }
+         }/>
         <>
           {displayedMonsters}
         </>
@@ -36,5 +53,4 @@ class App extends Component {
   }
 
 }
-
 export default App;
